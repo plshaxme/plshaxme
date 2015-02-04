075009badeab530fd20a845a15bf17bca18b1419
Steven Smith <Steveice10@gmail.com>
2015-02-03 18:05:44 -0800

    Run formatter.

--- source/main.cpp
+++ source/main.cpp
@@ -1,23 +1,23 @@
 #include <ctrcommon/common.hpp>
 
 int main(int argc, char **argv) {
-	if(!platform_init()) {
-		return 0;
-	}
+    if(!platform_init()) {
+        return 0;
+    }
 
-	while(platform_is_running()) {
-		input_poll();
-		if(input_is_pressed(BUTTON_START)) {
-			break;
-		}
+    while(platform_is_running()) {
+        input_poll();
+        if(input_is_pressed(BUTTON_START)) {
+            break;
+        }
 
-		screen_begin_draw(TOP_SCREEN);
-		screen_draw_string("Hello world!", (screen_get_width() - screen_get_str_width("Hello world!")) / 2, (screen_get_height() - screen_get_str_height("Hello world!")) / 2, 255, 255, 255);
-		screen_end_draw();
+        screen_begin_draw(TOP_SCREEN);
+        screen_draw_string("Hello world!", (screen_get_width() - screen_get_str_width("Hello world!")) / 2, (screen_get_height() - screen_get_str_height("Hello world!")) / 2, 255, 255, 255);
+        screen_end_draw();
 
-		screen_swap_buffers();
-	}
+        screen_swap_buffers();
+    }
 
-	platform_cleanup();
-	return 0;
+    platform_cleanup();
+    return 0;
 }
