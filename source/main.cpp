5499d663d26be75e1e526d3917c2d2257304d539
Steven Smith <Steveice10@gmail.com>
2015-03-20 06:54:10 -0700

    Update for ctrcommon changes.

--- source/main.cpp
+++ source/main.cpp
@@ -3,23 +3,23 @@
 #include <ctrcommon/screen.hpp>
 
 int main(int argc, char **argv) {
-    if(!platform_init()) {
+    if(!platformInit()) {
         return 0;
     }
 
-    while(platform_is_running()) {
-        input_poll();
-        if(input_is_pressed(BUTTON_START)) {
+    while(platformIsRunning()) {
+        inputPoll();
+        if(inputIsPressed(BUTTON_START)) {
             break;
         }
 
-        screen_begin_draw(TOP_SCREEN);
-        screen_draw_string("Hello world!", (screen_get_width() - screen_get_str_width("Hello world!")) / 2, (screen_get_height() - screen_get_str_height("Hello world!")) / 2, 255, 255, 255);
-        screen_end_draw();
+        screenBeginDraw(TOP_SCREEN);
+        screenDrawString("Hello world!", (screenGetWidth() - screenGetStrWidth("Hello world!")) / 2, (screenGetHeight() - screenGetStrHeight("Hello world!")) / 2, 255, 255, 255);
+        screenEndDraw();
 
-        screen_swap_buffers();
+        screenSwapBuffers();
     }
 
-    platform_cleanup();
+    platformCleanup();
     return 0;
 }
