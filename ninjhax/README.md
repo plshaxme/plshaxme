d08cf98110cc16307895c6e95ac93a02766e4823
leo60228 <iakornfeld@gmail.com>
2017-05-21 13:03:26 -0400

    removed ninjhax

--- ninjhax/README.md
+++
@@ -1,8 +0,0 @@
-ninjhax 2.x
-=======
-
-### Credits
-
-- All original ROP and code on this repo written by smea
-- yellows8, plutoo
-- mtheall for the compression code.
--- ninjhax/app_bootloader/source/takeover.h
+++
@@ -1,7 +0,0 @@
-#ifndef TAKEOVER_H
-#define TAKEOVER_H
-
-void getProcessMap(Handle fsuserHandle, u8 mediatype, u32 tid_low, u32 tid_high, void* out, u32* tmp);
-void superto(u64 tid, u32 mediatype, u32* argbuf, u32 argbuflength);
-
-#endif
